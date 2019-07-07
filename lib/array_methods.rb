63f28d67bd82273944723946cdb4706849732813
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 01:45:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,11 @@
 def find_element_index(array, value_to_find)
- # array.each_with_index do |nums, index|
-   if array.include?(value_to_find)
+   array.each_with_index do |nums, index|
+   if nums == value_to_find
     return index
    else
     puts "nil"
   end
+ end
 end
 
 def find_max_value(array)
