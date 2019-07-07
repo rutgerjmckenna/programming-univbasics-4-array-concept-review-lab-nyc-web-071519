bb50ad5a3abea7704d1b54e207a0b16f3263f16a
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 01:12:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
 def find_element_index(array, value_to_find)
   array.each_with_index |value_to_find, index|
+   if array.include?(value_to_find)
     puts "#{index}"
+   else
+    puts "nil"
 end
 
 def find_max_value(array)
