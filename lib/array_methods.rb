04d36695bff9a8c646436594fca01c189eb78c9b
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 00:48:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
+  if array.include?(value_to_find)
+    return value_to_find.index
+  else
+    return nil
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+ array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
