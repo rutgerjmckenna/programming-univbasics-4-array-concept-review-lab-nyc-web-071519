da4a90355a31182e19ee13a4d543580c98778897
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 01:50:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
+require "pry"
+
 def find_element_index(array, value_to_find)
    array.each_with_index do |nums, index|
    if nums == value_to_find
+     binding.pry
     return index
    else
     return nil
