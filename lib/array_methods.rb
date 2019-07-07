8e21f286a973c2cb22c632fa7b3725b6070ca491
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 01:43:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,6 @@
-require "pry"
-
 def find_element_index(array, value_to_find)
  # array.each_with_index do |nums, index|
    if array.include?(value_to_find)
-    binding.pry 
     return index
    else
     puts "nil"
