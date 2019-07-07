c51fa96272cd6db3d6bc09dcfc907720fb1d6b68
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 01:46:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-   array.each_with_index do |nums, index|
+   array.each_with_index do |index, nums|
    if nums == value_to_find
     return array.index(value_to_find)
    else
